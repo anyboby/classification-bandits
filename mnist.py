5c6b2f9031804b08eca232eb2a347f91dd186c4b
anyboby <zanger.moritz@googlemail.com>
2023-11-06 17:17:22 +0100

    moved dataloader to same file

--- mnist.py
+++ mnist.py
@@ -5,12 +5,71 @@ Adapted from https://github.com/google-deepmind/bsuite/tree/main/bsuite
 
 from bsuite.environments import base
 from bsuite.experiments.mnist import sweep
-from datasets import load_mnist
 
 import dm_env
 from dm_env import specs
 import numpy as np
 
+import array
+import gzip
+import os
+from os import path
+import struct
+
+import urllib.request
+
+_DATA = "/tmp/jax_example_data/"
+
+def _download(url, filename):
+  """Download a url to a file in the JAX data temp directory."""
+  if not path.exists(_DATA):
+    os.makedirs(_DATA)
+  out_file = path.join(_DATA, filename)
+  if not path.isfile(out_file):
+    urllib.request.urlretrieve(url, out_file)
+    print(f"downloaded {url} to {_DATA}")
+
+def mnist_raw():
+  """Download and parse the raw MNIST dataset."""
+  # CVDF mirror of http://yann.lecun.com/exdb/mnist/
+  base_url = "https://storage.googleapis.com/cvdf-datasets/mnist/"
+
+  def parse_labels(filename):
+    with gzip.open(filename, "rb") as fh:
+      _ = struct.unpack(">II", fh.read(8))
+      return np.array(array.array("B", fh.read()), dtype=np.uint8)
+
+  def parse_images(filename):
+    with gzip.open(filename, "rb") as fh:
+      _, num_data, rows, cols = struct.unpack(">IIII", fh.read(16))
+      return np.array(array.array("B", fh.read()),
+                      dtype=np.uint8).reshape(num_data, rows, cols)
+
+  for filename in ["train-images-idx3-ubyte.gz", "train-labels-idx1-ubyte.gz",
+                   "t10k-images-idx3-ubyte.gz", "t10k-labels-idx1-ubyte.gz"]:
+    _download(base_url + filename, filename)
+
+  train_images = parse_images(path.join(_DATA, "train-images-idx3-ubyte.gz"))
+  train_labels = parse_labels(path.join(_DATA, "train-labels-idx1-ubyte.gz"))
+  test_images = parse_images(path.join(_DATA, "t10k-images-idx3-ubyte.gz"))
+  test_labels = parse_labels(path.join(_DATA, "t10k-labels-idx1-ubyte.gz"))
+
+  return train_images, train_labels, test_images, test_labels
+
+
+def load_mnist(permute_train=False):
+  """Download, parse and process MNIST data to unit scale and one-hot labels."""
+  train_images, train_labels, test_images, test_labels = mnist_raw()
+
+  train_images = train_images / np.float32(255.)
+  test_images = test_images / np.float32(255.)
+  
+  if permute_train:
+    perm = np.random.RandomState(0).permutation(train_images.shape[0])
+    train_images = train_images[perm]
+    train_labels = train_labels[perm]
+
+  return (train_images, train_labels), (test_images, test_labels)
 
 class MNISTBandit(base.Environment):
   """MNIST classification as a bandit environment."""
